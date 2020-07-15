2078faa9ed055bd53b2b3ea19e4f6fc49d35600f
napisara <napisara.n@rmutsvmail.com>
2020-07-15 14:56:16 +0700

    add more code

--- src/Lab1/HelloWorld.java
+++ src/Lab1/HelloWorld.java
@@ -7,5 +7,10 @@ public class HelloWorld
 
 
         System.out.println("Hello world !!!");
+        System.out.println("Napisara Na Nakhon");
+        System.out.println("363211760034!!!");
+        System.out.println("สาขาการจัดการเทคโนโลยีสารสนเทศ");
+        System.out.println("คณะเทคโนโลยีการจัดการ");
+
     }
 }
