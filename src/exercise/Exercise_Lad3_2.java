a981d4bdf4df965a8c52b3190ecbab0c1889ecf9
napisara <napisara.n@rmutsvmail.com>
2020-08-19 15:51:16 +0700

    exercise lab3

---
+++ src/exercise/Exercise_Lad3_2.java
@@ -0,0 +1,15 @@
+
+package Exercise;
+import java.util.Scanner;
+public class Exercise_Lad3_2 {
+    public static void main(String[] args) {
+        for (int i =1; i <=10; i++) { // row
+            for (int j = 1; j <= 10 ; j++) { // column
+                System.out.print("*");
+
+
+            }//j
+            System.out.println();
+        }//i
+    }
+}
