4f1adf273564c8b5d2149dafff494390cd8bebeb
napisara <napisara.n@rmutsvmail.com>
2020-10-07 15:35:32 +0700

    exercise 8

---
+++ src/exercise/Exercise_Lab8_2.java
@@ -0,0 +1,18 @@
+package Exerise;
+import java.util.Scanner;
+public class Exercise_Lab8_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a String: ");
+        String s = sc.nextLine();
+        //String
+        int x =0;
+        try {
+            x = Integer.parseInt(s);
+        }catch (NumberFormatException e){
+            System.out.println("Can not transfer String to int. ");
+        }
+        System.out.println(x);
+
+    }
+}
