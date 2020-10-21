d949ebb87b73a7cb439fbf6991f92f4dc17a8f67
napisara <napisara.n@rmutsvmail.com>
2020-10-21 12:57:30 +0700

    exercise 9

---
+++ src/Lab9/ExWriteFile.java
@@ -0,0 +1,22 @@
+package Lab9;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+public class ExWriteFile {
+    public static void main(String[] args) {
+        Path pFile = Paths.get("myname.txt");
+        try {
+            BufferedReader reader = Files.newBufferedReader(pFile, StandardCharsets.UTF_8);
+            String msg  =null;
+            System.out.println("Text form file: ");
+            while ((msg = reader.readLine())!=null){
+                System.out.println(msg);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
